0a90a7a0d6b06554694e6078060d846cc00d381b
443148514@qq.com <443148514@qq.com>
2019-03-22 00:03:59 +0800

    add
    add

--- feign/feign-service/src/main/java/com/springcloud/feign/FeignServiceApplication.java
+++ feign/feign-service/src/main/java/com/springcloud/feign/FeignServiceApplication.java
@@ -3,13 +3,11 @@ package com.springcloud.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author mr.yang
  * @date 2019/3/2
  */
-@EnableDiscoveryClient
 @SpringBootApplication
 public class FeignServiceApplication {
     public static void main(String[] args) {
